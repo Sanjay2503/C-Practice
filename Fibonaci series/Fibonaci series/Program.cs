3fe1b81f779e38e6c9c5e1dc1b0ece3ccac6e1f5
Sanjay <sanju.github@gmail.com>
2022-06-06 19:22:00 +0530

    Fibonaci

---
+++ Fibonaci series/Fibonaci series/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Fibonaci
+{
+  public  class Fibonaci
+    {
+        
+      public  static void Main(string[] args)
+        {
+            int number, n1 = 0, n2 = 1, n3;
+            Console.WriteLine("Enter number how many number you want");
+            number = int.Parse(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2+ " ");
+
+            for(int i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
